cb53523040cb454239e7ab28390c1e98b5324f51
bgomezdh <bgomez@digitalhouse.com>
2022-10-06 09:39:07 -0300

    Ad the controller methods

--- day11-models/myApp/controllers/movieController.js
+++ day11-models/myApp/controllers/movieController.js
@@ -1,11 +1,54 @@
 /* Importar */
-const db = require('../db/data');
+const db = require('../database/models');
+const movie = db.Movie;
+
+const op = db.Sequelize.Op;
 
 /* Desarrollar */
 const movieController = {
     index : function(req, res) {
 
-        return res.render('movies', {peliculas : db.lista});
+            let criterios = {
+               /*  where : [{awards : 1}, {length : 120}],
+                order : [["title", "DESC"]],
+                limit : 2 */
+            }
+
+            movie.findAll(criterios)
+            .then((result) => {
+                return res.render("movies", {peliculas : result})
+            });
+        
+    },
+
+    show: (req, res) =>{
+        let id = req.params.id;
+
+        movie.findByPk(id)
+        .then((result) => {
+            return res.render("detalleMovies", {movie : result})
+        })
+        .catch();
+    },
+
+    showOne : (req, res) => {
+        let busqueda = req.query.pelicula;
+
+        let criterios = {
+            where : [
+                /* {title: busqueda} */
+                {title: {[op.like] : "%" + busqueda + "%"}}
+            ]
+        }
+
+        movie.findOne(criterios)
+        .then((result) => {
+            return res.render("detalleMovies", {movie : result})
+        })
+        .catch((err) =>{
+            return res.redirect("/")
+        });
+        
     }
 }
 
--- day11-models/myApp/routes/movie.js
+++ day11-models/myApp/routes/movie.js
@@ -5,4 +5,8 @@ const movieController = require('../controllers/movieController');
 /* GET home page. */
 router.get('/', movieController.index);
 
+router.get('/detalle/:id', movieController.show);
+
+router.get('/busqueda', movieController.showOne);
+
 module.exports = router;
